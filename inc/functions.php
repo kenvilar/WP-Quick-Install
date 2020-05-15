4bd48bdc6f3ba87296d537e5ca01a531e3989e12
Ken Vilar <kenvilar@gmail.com>
2020-05-15 10:21:28 +0800

    Reformat code with wordpress style

--- inc/functions.php
+++ inc/functions.php
@@ -1,13 +1,11 @@
 <?php
 
-if (!function_exists('_')) {
-    function _($str)
-    {
-        echo $str;
-    }
+if ( ! function_exists( '_' ) ) {
+	function _( $str ) {
+		echo $str;
+	}
 }
 
-function sanit($str)
-{
-    return addcslashes(str_replace(array(';', "\n"), '', $str), '\\');
+function sanit( $str ) {
+	return addcslashes( str_replace( array( ';', "\n" ), '', $str ), '\\' );
 }
