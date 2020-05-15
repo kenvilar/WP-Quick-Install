b4304459aa82327dbd465a460e7700bc975a4e6d
Ken Vilar <kenvilar@gmail.com>
2020-05-15 10:17:36 +0800

    Reformat code

--- inc/functions.php
+++ inc/functions.php
@@ -1,11 +1,13 @@
 <?php
 
-if ( ! function_exists( '_' ) ) {
-	function _( $str ) {
-		echo $str;
-	}
+if (!function_exists('_')) {
+    function _($str)
+    {
+        echo $str;
+    }
 }
 
-function sanit( $str ) {
-	return addcslashes( str_replace( array( ';', "\n" ), '', $str ), '\\' );
+function sanit($str)
+{
+    return addcslashes(str_replace(array(';', "\n"), '', $str), '\\');
 }
